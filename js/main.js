188708f8c2e1bb7c67852d8e4d685081725f6b95
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-08 15:32:17 +0100

    feat: milestone

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 `use strict`;
+
+for (let contenutoCaselle = 1; contenutoCaselle <= 100; contenutoCaselle++) {
+  if (contenutoCaselle % 15 === 0) {
+    const multiplo3E5 = contenutoCaselle;
+    console.log(multiplo3E5, 'FizzBuzz');
+  } else if (contenutoCaselle % 5 === 0) {
+    const multiplo5 = contenutoCaselle;
+    console.log(multiplo5, 'Buzz');
+  } else if (contenutoCaselle % 3 === 0) {
+    const multiplo3 = contenutoCaselle;
+    console.log(multiplo3, 'Fizz');
+  }
+}
