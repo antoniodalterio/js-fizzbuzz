c75ff9bb79ee41c0b7edec2cdf13bc457cb06e6c
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-08 16:45:37 +0100

    feat: bonus

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 `use strict`;
 
 for (let contenutoCaselle = 1; contenutoCaselle <= 100; contenutoCaselle++) {
+  // contenitore
+  const contenitoreDiv = document.getElementById('contenitore-box');
+  const box = document.createElement('div');
+
+  box.classList.add('m-3');
+  box.classList.add('square');
+  box.append(contenutoCaselle);
+  contenitoreDiv.append(box);
+
   if (contenutoCaselle % 15 === 0) {
+    // multiplo di 3 e 5
     const multiplo3E5 = contenutoCaselle;
-    console.log(multiplo3E5, 'FizzBuzz');
+    box.append('FizzBuzz');
+    box.classList.add('bg-danger');
+    contenitoreDiv.append(box);
   } else if (contenutoCaselle % 5 === 0) {
+    // multiplo di 5
     const multiplo5 = contenutoCaselle;
-    console.log(multiplo5, 'Buzz');
+    box.append('Buzz');
+    box.classList.add('bg-warning');
+    contenitoreDiv.append(box);
   } else if (contenutoCaselle % 3 === 0) {
+    // multiplo di 3
     const multiplo3 = contenutoCaselle;
-    console.log(multiplo3, 'Fizz');
+    box.classList.add('bg-success');
+    box.append('Fizz');
   }
 }
